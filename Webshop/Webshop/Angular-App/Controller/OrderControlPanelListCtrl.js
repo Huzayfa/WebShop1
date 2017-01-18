c4bcc5ff34a55d0350e26970d7a0a7c7135a4b9b
munzerawad <monther.awad@yahoo.com>
2017-01-18 11:47:02 +0100

    edit

--- Webshop/Webshop/Angular-App/Controller/OrderControlPanelListCtrl.js
+++ Webshop/Webshop/Angular-App/Controller/OrderControlPanelListCtrl.js
@@ -28,14 +28,12 @@ app.controller('OrderControlPanelListCtrl', function ($window, $scope, $http, $t
             if (index > -1) {
                 $scope.ordersList.splice(index, 1);
             }
-        }
-        
-        
+        }       
     };
 
-    //get User By Id to Edit hem
+    //get order By Id to Edit it
     $scope.EditOrder = function (orderId) {
-       // $scope.selectedOrder = orderServices.getOrderDetails(orderId);
+        $scope.selectedOrder = orderServices.getOrderDetails(orderId);
         $scope.editSelected = true;
         $scope.detailsSelected = false;
         $scope.selectedOrderEditId = angular.copy(orderId);
