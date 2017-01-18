bf794dbfff1f31f9d6001f424d319815ef7141a3
munzerawad <monther.awad@yahoo.com>
2017-01-18 15:57:48 +0100

    Delete not importantfile

--- Webshop/Webshop/Properties/Angular-App/App/WebShop.js
+++
@@ -1,1 +0,0 @@
-﻿var app = angular.module("WebShop", ["ngRoute", "ngCookies"]);
--- Webshop/Webshop/Properties/Angular-App/Service/CookieOptionService.js
+++
@@ -1,6 +0,0 @@
-﻿app.service("cookieOptionService", function () {
-    this.cookieName = "customerCart";
-    var efterDays = 1;
-    var now = new Date();
-    this.exp = new Date(now.getFullYear(), now.getMonth(), now.getDate() ,now.getMinutes+ efterDays);
-});
