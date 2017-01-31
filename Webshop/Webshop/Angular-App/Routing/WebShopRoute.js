fbe0ab055e81112b1ef1a8688e522df7595fa8c9
Lennart Kråksberg <lennartkr@hotmail.com>
2017-01-31 09:47:16 +0100

    Orderlines

--- Webshop/Webshop/Angular-App/Routing/WebShopRoute.js
+++ Webshop/Webshop/Angular-App/Routing/WebShopRoute.js
@@ -26,6 +26,8 @@ app.config(function ($routeProvider) {
         templateUrl:"/Account/Login"
     }).when("/Orders", {
         templateUrl: "/Order/"
+    }).when("/OrderLines", {
+        templateUrl: "/OrderLines/"
     }).when("/Register", {
         templateUrl: "/RegisterUser/"
 
