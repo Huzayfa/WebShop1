200363b7150fa321eb87ce412b022cbb5b65e42b
munzerawad <monther.awad@yahoo.com>
2017-02-03 14:53:31 +0100

    Emit Load

--- Webshop/Webshop/Angular-App/Controller/RecommendedProductCtrl.js
+++ Webshop/Webshop/Angular-App/Controller/RecommendedProductCtrl.js
@@ -3,13 +3,15 @@
     $scope.recommedndedProductsList = [];
     
     //$("body").css("cursor", "progress");
-    $("body").css("cursor", "wait");
+    //$("body").css("cursor", "wait");
+    $scope.$emit("Load");
     $scope.carouselSlide = "";
     $http.get("/Product/RecommedndedProducts", { cache: false }).then(function (response) {
 
         angular.copy(response.data, $scope.recommedndedProductsList);
-        $("body").css("cursor", "default");
+        //$("body").css("cursor", "default");
         $scope.carouselSlide = "carousel slide";
+        $scope.$emit("UNLoad");
     }
     );
 
