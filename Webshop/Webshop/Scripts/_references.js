50275b2af1ab11db06b6d86da0f5974c9506b7c5
munzerawad <monther.awad@yahoo.com>
2017-03-16 15:56:24 +0100

    Css

--- Webshop/Webshop/Scripts/_references.js
+++ Webshop/Webshop/Scripts/_references.js
@@ -56,6 +56,7 @@
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="jquery-3.1.1.js" />
 /// <reference path="jquery-3.1.1.slim.js" />
+/// <reference path="jquery-migrate-3.0.0.min.js" />
 /// <reference path="jquery-ui-1.12.1.js" />
 /// <reference path="lodash.js" />
 /// <reference path="modernizr-2.6.2.js" />
