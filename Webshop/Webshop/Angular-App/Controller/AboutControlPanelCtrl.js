d4b60c103551b3a3b59ecf6b9a00ebd21969594b
munzerawad <monther.awad@yahoo.com>
2017-03-30 14:54:36 +0200

    Css

--- Webshop/Webshop/Angular-App/Controller/AboutControlPanelCtrl.js
+++ Webshop/Webshop/Angular-App/Controller/AboutControlPanelCtrl.js
@@ -1,11 +1,24 @@
 ﻿
 'use strict'
-app.controller('AboutControlPanelCtrl', function ($window, $scope, $http, $timeout) {
-
-    $scope.about = "About us ";
-    console.log($scope.about);
+app.controller('AboutControlPanelCtrl', function ($sce, $scope, $http) {
 
 
+    $scope.data = {
+        text: $sce.trustAsHtml("hello"),
+    }
+
+
+
+
+    $http.get("/About/GetAboutUsInformations", { cache: false }).then(function (response) {
+        //angular.copy(response.data.Content, $scope.data.text);
+        $scope.data.text = $sce.trustAsHtml(response.data.Content);
+        //$scope.data.text = response.data.Content;
+
+
+    }
+
+     );
 
 }
 );
--- Webshop/Webshop/Angular-App/Controller/AboutusControlPanelCtrl.js
+++ Webshop/Webshop/Angular-App/Controller/AboutusControlPanelCtrl.js
@@ -12,8 +12,9 @@ app.controller('AboutUsControlPanelCtrl', function ($sce,$scope, $http) {
 
     $http.get("/About/GetAboutUsInformations", { cache: false }).then(function (response) {
         //angular.copy(response.data.Content, $scope.data.text);
-        $scope.data.text = $sce.trustAsHtml(response.data.Content);
-        console.log($scope.data.text);
+        //$scope.data.text = $sce.trustAsHtml(response.data.Content);
+        $scope.data.text =response.data.Content;
+       
        
     }
 
@@ -26,8 +27,7 @@ app.controller('AboutUsControlPanelCtrl', function ($sce,$scope, $http) {
     {
         console.log($scope.data.text);
         $http.post('About/EditAboutUsData', JSON.stringify({ data: $scope.data.text })).then(function (response) {
-
-            console.log(data.text);
+ 
         });
     }
 
